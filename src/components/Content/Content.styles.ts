652a39e5dca2721bb817e0f2a863906634079c33
ireshka <ireshka@users.noreply.github.com>
2021-05-22 05:23:18 +0200

    feat(rwd): change layout on mobile devices

--- src/components/Content/Content.styles.ts
+++ src/components/Content/Content.styles.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Wrapper = styled('div')`
   align-items: center;
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-row-gap: 50px;
+  grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
   justify-items: center;
 `;
